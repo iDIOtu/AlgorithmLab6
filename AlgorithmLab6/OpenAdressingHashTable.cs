f016e9e10c4feeda1746acf4928c1e703210a475
Alexander <alex@msmirnov.net>
2024-12-22 19:17:31 +0500

    Ну типа, таблица с открытой адресацией

--- AlgorithmLab6/OpenAdressingHashTable.cs
+++ AlgorithmLab6/OpenAdressingHashTable.cs
@@ -2,8 +2,134 @@
 
 namespace AlgorithmLab6
 {
-    internal class OpenAddressingHashTable<T1, T2> : IHashTable<T1, T2>
+    internal class OpenAddressingHashTable<T1, T2> : IHashTable<T1, T2> where T1 : IComparable
     {
+        private const int Size = 10000;
+        private (T1 key, T2 value)?[] table;
+        private int count;
+
+        public OpenAddressingHashTable()
+        {
+            table = new (T1, T2)?[Size];
+            count = 0;
+        }
+
+        // Метод хеширования
+        private int Hash(int key)
+        {
+            return key % Size; 
+        }
+
+        private int GenericHash(T1 key)
+        {
+            return Math.Abs(key.GetHashCode()) % Size;
+        }
+
+        // Линейное исследование
+        private int LinearProbing(int key, int i)
+        {
+            return (Hash(key) + i) % Size;
+        }
+
+        // Квадратичное исследование
+        private int QuadraticProbing(int key, int i)
+        {
+            return (Hash(key) + i * i) % Size;
+        }
+
+        // Двойное хеширование
+        private int DoubleHash(int key, int i)
+        {
+            int hash2 = 7 - (key % 7); // Вторичный хеш
+            return (Hash(key) + i * hash2) % Size;
+        }
+
+        // Вставка элемента
+        public void Insert(T1 key, T2 value, string probingMethod = "linear")
+        {
+            if (count >= Size)
+            {
+                throw new InvalidOperationException("Hash table is full");
+            }
+
+            int i = 0;
+            int index;
+            int hash = GenericHash(key);
+            do
+            {
+                switch (probingMethod)
+                {
+                    case "linear":
+                        index = LinearProbing(hash, i);
+                        break;
+                    case "quadratic":
+                        index = QuadraticProbing(hash, i);
+                        break;
+                    case "double":
+                        index = DoubleHash(hash, i);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid probing method");
+                }
+
+                if (!table[index].HasValue) // 
+                {
+                    table[index] = (key, value);
+                    count++;
+                    return;
+                }
+
+                i++;
+            } while (true);
+        }
+
+        // Поиск элемента
+        //public bool Search(int key)
+        //{
+        //    int i = 0;
+        //    int index;
+        //    do
+        //    {
+        //        index = LinearProbing(key, i); // Можно выбрать любой метод
+
+        //        if (table[index] == null)
+        //        {
+        //            return false; // Элемент не найден
+        //        }
+
+        //        if (table[index] == key)
+        //        {
+        //            return true; // Элемент найден
+        //        }
+
+        //        i++;
+        //    } while (true);
+        //}
+
+        // Удаление элемента
+        //public void Delete(int key)
+        //{
+        //    int i = 0;
+        //    int index;
+        //    do
+        //    {
+        //        index = LinearProbing(key, i); // Можно выбрать любой метод
+
+        //        if (table[index] == null)
+        //        {
+        //            throw new InvalidOperationException("Element not found");
+        //        }
+
+        //        if (table[index] == key)
+        //        {
+        //            table[index] = -1; // Помечаем элемент как удалённый
+        //            count--;
+        //            return;
+        //        }
+
+        //        i++;
+        //    } while (true);
+        //}
         public T2 Add(T1 key)
         {
             throw new NotImplementedException();
@@ -14,9 +140,54 @@ namespace AlgorithmLab6
             throw new NotImplementedException();
         }
 
+        public bool Find(T1 key, out T2 value) // public bool Search(T1 key, out T2 value)
+        {
+            int i = 0;
+            int hash = GenericHash(key);
+            int index;
+            do
+            {
+                index = LinearProbing(hash, i);
+
+                if (!table[index].HasValue)
+                {
+                    value = default;
+                    return false; // Элемент не найден
+                }
+
+                if (table[index].Value.key.Equals(key))
+                {
+                    value = table[index].Value.value; // Элемент найден
+                    return true;
+                }
+
+                i++;
+            } while (true);
+        }
+
         public void Remove(T1 key)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            int hash = GenericHash(key);
+            int index;
+            do
+            {
+                index = LinearProbing(hash, i);
+
+                if (!table[index].HasValue)
+                {
+                    throw new InvalidOperationException("Element not found");
+                }
+
+                if (table[index].Value.key.Equals(key))
+                {
+                    table[index] = null; // Удаляем элемент
+                    count--;
+                    return;
+                }
+
+                i++;
+            } while (true);
         }
     }
 }
--- AlgorithmLab6/Program.cs
+++ AlgorithmLab6/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace AlgorithmLab6
+﻿using System.Collections;
+using System;
+
+namespace AlgorithmLab6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            OpenAddressingHashTable<string, string> hashTable = new OpenAddressingHashTable<string, string>();
+            // Вставка элементов
+            hashTable.Insert("2", "1");
+            hashTable.Insert("2", "2");
+            hashTable.Insert("3", "3");
+
+            // Поиск элементов
+            Console.WriteLine(hashTable.Find("2", out string foundedValue)); // True
+            Console.WriteLine(hashTable.Find("4", out foundedValue)); // False
+
+            // Удаление элемента
+            hashTable.Remove("2");
+            Console.WriteLine(hashTable.Find("2", out foundedValue)); // False
         }
     }
 }
